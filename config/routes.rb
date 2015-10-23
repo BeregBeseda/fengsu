b757a57f38a00b3f97c55c35ee023b3c51c8e168
VitalinaLiane <vitalina.liane@gmail.com>
2015-10-23 18:07:29 +0300

    ROUTES: match to nonexist paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
-Rails.application.routes.draw do
-
-  match '/', to: 'menus#index', via: 'get'  
-  match 'about/', to: 'menus#index', via: 'get'          
-  match 'about/:translit', to: 'orders#new', via: 'get'      
+Rails.application.routes.draw do              
+  match '/',                                             to: 'menus#index',   via: 'get'  
+  match 'about/',                                        to: 'menus#index',   via: 'get'          
+  match 'about/:translit',                               to: 'orders#new',    via: 'get'      
   
   root 'menus#index'
     
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)    
+  
+  
+  match "*path", to: 'menus#index',   via: 'get'
+  
+  #match '/:random1',                                     to: 'menus#index',   via: 'get'
+  #match '/:random1/:random2',                            to: 'menus#index',   via: 'get'  
+  #match '/:random1/:random2/:random3',                   to: 'menus#index',   via: 'get'      
+  #match '/:random1/:random2/:random3/:random4',          to: 'menus#index',   via: 'get'        
+  #match '/:random1/:random2/:random3/:random4/:random5', to: 'menus#index',   via: 'get'
 end
