b7ceff1a13b95ebc06e2d2b2ea470f0f49561124
VitalinaLiane <vitalina.liane@gmail.com>
2015-12-04 22:04:50 +0200

    single order is done

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Fengsu"
+  config.site_title = "Фен-Шуй-консультация онлайн по Скайпу"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -33,7 +33,7 @@ ActiveAdmin.setup do |config|
   #   config.default_namespace = false
   #
   # Default:
-  # config.default_namespace = :admin
+  config.default_namespace = :fairytale
   #
   # You can customize the settings for each namespace by using
   # a namespace block. For example, to change the site title
