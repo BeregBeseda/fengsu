222d3f03e5ba804ea694de1147f1bc128440128c
VitalinaLiane <vitalina.liane@gmail.com>
2016-03-10 00:56:15 +0200

    LP response 07 - data_hash with status via data decode_json

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -88,7 +88,7 @@ class OrdersController < ApplicationController
     private_key = 'irj04vFv5A7g7pdVVdJ59ja5nh79U5IlylVQk8jQ'
     data = params[:data]
      
-    data_hash = decode_json decode64 data
+    data_hash = decode_json data
     #data_hash = decode_json data  
         
     liqpay = Liqpay::Liqpay.new(
