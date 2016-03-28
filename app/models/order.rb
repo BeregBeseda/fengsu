1d5c0ebf0d1765eb3fc1bb8869d09b27c9876a20
VitalinaLiane <vitalina.liane@gmail.com>
2016-03-28 15:09:47 +0300

    3 check mark on order? #2

--- app/models/order.rb
+++ app/models/order.rb
@@ -26,24 +26,4 @@ class Order < ActiveRecord::Base
                                              
                            :on => :create                
 
-#*********************************************************************************************************************************************  
-
-  validates :when_payed,   presence:       { message:       'please, enter date and time when you have payed' },        
-                                             
-                           length:         { maximum:        100, 
-                                             too_long:      'when payed line is too long',
-                                             :if =>          :when_payed? },
-                           :on => :update
-                           
-  validates :end_cards,    presence:       { message:       'please, enter 2 last digits of card' },        
-                                             
-                           numericality:   { only_integer:   true,
-                                             message:        'is not a number ' },                  
-                                             
-                           length:         { is:             2, 
-                                             wrong_length:  'wrong length',                                             
-                                             :if =>         :end_cards? },   
-                                             
-                           :on => :update
-
 end
