0e6f00953917fa1161b7df9f9009190c78af2eff
VitalinaLiane <vitalina.liane@gmail.com>
2016-04-17 01:40:41 +0300

    Added prerendering (cache) -> test cool via Validator?

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,6 +2,8 @@ class MenusController < ApplicationController
   before_action :set_menus, only: [:index]
   
   def index 
+    root_path = MeConstant.find_by_title('root_path').content  
+    @prerender_page = root_path + 'about/lichnaya-zhizn'
   end
   
   
