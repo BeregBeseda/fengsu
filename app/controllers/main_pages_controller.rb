f4e401a905b5861aaed3d16d5bbbe88d5a95ba03
VitalinaLiane <vitalina.liane@gmail.com>
2016-04-20 08:56:51 +0300

    Test03

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -16,9 +16,9 @@ class MainPagesController < ApplicationController
     
     
     #link_details_encoded = '123456asdj717866c554731kjnq71n20s78f65q123456asd'
-    key_pair  = RSA::KeyPair.generate(500)
-    private_key_params = key_pair.private_key
-    @result = RSA::Key.initialize()
+    ##key_pair  = RSA::KeyPair.generate(500)
+    ##private_key_params = key_pair.private_key
+    ##@result = RSA::Key.initialize()
     #link_details_begin_ascii_8 = key_pair.encrypt(link_details_encoded)
     #@result = URI.encode(link_details_begin_ascii_8)
     
