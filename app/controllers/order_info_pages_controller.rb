57d8bf7d4971ce1ef157e95c1a2f1d4b7c04f094
VitalinaLiane <vitalina.liane@gmail.com>
2016-04-21 16:45:09 +0300

    04

--- app/controllers/order_info_pages_controller.rb
+++ app/controllers/order_info_pages_controller.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class OrderInfoPagesController < ApplicationController
 
-  before_action :set_page_and_order_info_page, :set_site_title,  only: [:show]
+  before_action :set_pages_and_order_info_page, :set_site_title,  only: [:show]
 
   def show
   end
@@ -12,16 +12,17 @@ class OrderInfoPagesController < ApplicationController
   private
   
     
-  def set_page_and_order_info_page
-    @page       = Page.find_by_page :info
+    def set_pages_and_order_info_page
+      @page       = Page.find_by_page :info
+      @main_page  = MainPage.find(1)       
   
-    msg         = params[:msg]    
-    @info_msg   = (OrderInfoPage.find_by translit: msg).msg
-  end  
+      msg         = params[:msg]    
+      @info_msg   = (OrderInfoPage.find_by translit: msg).msg
+    end  
     
     
-  def set_site_title 
-    @site_title = MeConstant.find_by_title('site_title').content          
-  end                                                 
+    def set_site_title 
+      @site_title = MeConstant.find_by_title('site_title').content          
+    end                                                 
     
 end
